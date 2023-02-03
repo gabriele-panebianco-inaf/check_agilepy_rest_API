93fc5bf0596c2d6c431a45030ad132add1f1e773
gabriele.panebianco <gabriele.panebianco@inaf.it>
2023-02-03 19:17:51 +0100

    Check Target 4 completed

--- Execute_scripts.sh
+++ Execute_scripts.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # python WriteConfFile.py -t 0
+# python DownloadData.py -t 0 > ./Logs/0_DownloadData.log
+
 # python WriteConfFile.py -t 1
+# python DownloadData.py -t 1 > ./Logs/1_DownloadData.log
+
 # python WriteConfFile.py -t 2
+# python DownloadData.py -t 2 > ./Logs/2_DownloadData.log
+
 # python WriteConfFile.py -t 3
+# python DownloadData.py -t 3 > ./Logs/3_DownloadData.log
+# python DataAnalysis.py -t 3
 
-#python DownloadData.py -t 0 > ./Logs/0_DownloadData.log
-#python DownloadData.py -t 1 > ./Logs/1_DownloadData.log
-#python DownloadData.py -t 2 > ./Logs/2_DownloadData.log
-#python DownloadData.py -t 3 > ./Logs/3_DownloadData.log
-
-#python DataAnalysis.py -t 3
-
-python WriteConfFile.py -t 4 
+# python WriteConfFile.py -t 4
+# python DownloadData.py -t 4 > ./Logs/4_DownloadData.log
+python DataAnalysis.py -t 4
